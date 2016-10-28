f2cec827a4eefa7307d7c858c42ab690be307100
Graham Coombe <gscoombe@gmail.com>
2016-10-27 22:30:22 -0700

    Ability to run test modules individually

--- run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd server
-python -m unittest discover -v
--- server/tests/run_tests.py
+++ server/tests/run_tests.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 import unittest
+from os import sys, path
 
-def main():
+def main(modules=None):
     loader = unittest.TestLoader()
-    test_suite = loader.discover('.')
+    if modules:
+        test_modules = ['test_' + x for x in modules]
+        test_suite = loader.loadTestsFromNames(test_modules)
+    else:
+        # Run them all
+        test_suite = loader.discover('.')
 
     test_runner = unittest.runner.TextTestRunner(verbosity=1)
     test_runner.run(test_suite)
 
+if __name__ == '__main__' and __package__ is None:
+    # Add root path to sys.path for module discovery.
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
 if __name__ == '__main__':
-    main()
+    # Accept module name arguments
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
+    else:
+        main()
