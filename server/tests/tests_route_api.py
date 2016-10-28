3c1411be87ef4a95c0bc6c884a6aaba5523dba5d
Graham Coombe <gscoombe@gmail.com>
2016-10-27 19:15:45 -0700

    update tests after changes to osmparser api

--- server/tests/tests_route_api.py
+++ server/tests/tests_route_api.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 import app as heatmapper_app
 import json
 from osmparser.graph import Graph, Node, Edge
+from views import route_api
 
 class RoutApiTestCase(unittest.TestCase):
 
@@ -15,13 +16,14 @@ class RoutApiTestCase(unittest.TestCase):
     #     os.unlink(flaskr.app.config['DATABASE'])
 
     def generate_simple_graph(self):
-        nodes = {"1": Node(49.278868, -123.125652), "2": Node(49.279932, -123.124053), "3": Node(49.279281, -123.123034)};
-        edges = [Edge("1", "2", 1), Edge("2", "3", 1)]
+        nodes = {"1": Node("1", 49.278868, -123.125652), "2": Node("2", 49.279932, -123.124053), "3": Node("3", 49.279281, -123.123034)};
+        edges = [Edge("e1", [nodes["1"], nodes["2"]], 1), Edge("e2", [nodes["2"], nodes["3"]], 1)]
         return Graph(nodes, edges)
 
     def generate_disconnected_graph(self):
-        nodes = {"1": Node(49.278868, -123.125652), "2": Node(49.379932, -123.224053), "3": Node(49.479281, -123.323034), "4": Node(49.579281, -123.523034)};
-        edges = [Edge("1", "2", 1), Edge("3", "4", 1)]
+        nodes = {"1": Node("1", 49.278868, -123.125652), "2": Node("2", 49.279932, -123.124053), "3": Node("3", 49.279281, -123.123034), "4": Node("4", 49.579281, -123.523034)};
+        edges = [Edge("e1", [nodes["1"], nodes["2"]], 1), Edge("e2", [nodes["3"], nodes["4"]], 1)]
+
         return Graph(nodes, edges)
 
     def mock_returns(self, mock_graph_fetcher, sucessfull_path=True):
@@ -62,9 +64,8 @@ class RoutApiTestCase(unittest.TestCase):
         data = json.loads(rv.get_data(as_text=True))["result"]
         self.assertEqual(data["status"], "fail")
         self.assertIsNone(data["path"])
-        self.assertCountEqual(data["nodes"], [{"lat": 49.278868, "lng": -123.125652}, {"lat":49.379932, "lng": -123.224053}, {"lat": 49.479281, "lng": -123.323034}, {"lat": 49.579281, "lng": -123.523034}])
-
-
+        print("NODES", data["nodes"])
+        self.assertCountEqual(data["nodes"], [{"lat": 49.278868, "lng": -123.125652}, {"lat": 49.279932, "lng": -123.124053}, {"lat": 49.279281, "lng": -123.123034}, {"lat":49.579281, "lng": -123.523034}])
 
 if __name__ == '__main__':
     unittest.main()
